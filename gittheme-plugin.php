066ea4bba0690d0bf3c5d25e52ba0414083c09bb
Naeem Prasla <mhkprasla@gmail.com>
2024-11-25 07:22:13 +0500

    readme-updates

--- gittheme-plugin.php
+++ gittheme-plugin.php
@@ -4,6 +4,10 @@ Plugin Name: Git Theme Sync (Wp Theme)
 Description: Sync GitHub Wp Theme with wordpress
 Version: 1.4
 Author: Naeem Prasla
+Requires at least: 4.7
+Tested up to: 5.4
+Stable tag: 4.3
+Requires PHP: 7.0
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
